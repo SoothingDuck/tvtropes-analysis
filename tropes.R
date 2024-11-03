0513750c706d1a9700716b5ba7c9010d9623ff1d
Orpheus <orpheus@gmail.com>
2024-11-03 18:53:25 +0100

    GG

--- tropes.R
+++ tropes.R
@@ -3,22 +3,65 @@ library(rvest)
 library(tidyverse)
 library(stringr)
 
-## Fonctions Categories
-get_subpage_links_from_page <- function(page) {
+## Fonctions Main article
+
+main - article
+
+mainarticle_links_from_page <- function(page) {
     session <- bow("https://tvtropes.org/pmwiki/pmwiki.php/WebVideo/TheOldestView", force = TRUE)
 
     result <- scrape(session, query = list(t = "semi-soft", per_page = 100))
 
     tmp <- tibble(
-        subpage_name = result %>% html_node(".subpage-links") %>% html_nodes("a") %>% html_text() %>% str_trim(),
-        subpage_ref = result %>% html_node(".subpage-links") %>% html_nodes("a") %>% html_attr("href") %>% paste0("https://tvtropes.org", .)
-    ) %>% filter(subpage_name != "Create Subpage")
+        main_article_href = result %>%
+            html_node("#main-article") %>%
+            html_nodes("a.twikilink") %>%
+            html_attr("href") %>%
+          paste0("https://tvtropes.org", .)
+    ) %>%
+        unique() %>%
+        mutate(main_article_name = str_split_i(main_article_href, "/", i = -1)) %>%
+        select(main_article_name, main_article_href)
+
+    return(tmp)
+}
+
+
+tmp <- tibble(
+    subpage_name = ,
+    subpage_ref = result %>%
+        html_node(".subpage-links") %>%
+        html_nodes("a") %>%
+        html_attr("href") %>%
+        paste0("https://tvtropes.org", .)
+) %>%
+    filter(subpage_name != "Create Subpage")
+
+## Fonctions Categories
+subpage_links_from_page <- function(page) {
+    session <- bow(page, force = TRUE)
+
+    result <- scrape(session, query = list(t = "semi-soft", per_page = 100))
+
+    tmp <- tibble(
+        subpage_name = result %>%
+            html_node(".subpage-links") %>%
+            html_nodes("a") %>%
+            html_text() %>%
+            str_trim(),
+        subpage_ref = result %>%
+            html_node(".subpage-links") %>%
+            html_nodes("a") %>%
+            html_attr("href") %>%
+            paste0("https://tvtropes.org", .)
+    ) %>%
+        filter(subpage_name != "Create Subpage")
 
     return(tmp)
 }
 
 ## Fonctions Bottom Links
-get_bottom_links_from_page <- function(page, col_number) {
+bottom_links_from_page <- function(page, col_number) {
     ## Retourne les pages "Index" en bas de page
     session <- bow(page, force = TRUE)
 
@@ -26,32 +69,40 @@ get_bottom_links_from_page <- function(page, col_number) {
 
     tmp <- tibble(
         link_name = (result %>% html_node(".links") %>% html_nodes("a") %>% html_text()),
-        link_ref = (result %>% html_node(".links") %>% html_nodes("a") %>% html_attr("href") %>% paste0("https://tvtropes.org", .))
-    ) %>% filter(as.numeric(rownames(.)) %% 3 == col_number)
+        link_ref = (result %>%
+            html_node(".links") %>%
+            html_nodes("a") %>%
+            html_attr("href") %>%
+            paste0("https://tvtropes.org", .)
+        )
+    ) %>%
+        filter(as.numeric(rownames(.)) %% 3 == col_number)
 
     return(tmp)
 }
 
-get_previous_links_from_page <- function(page) {
+previous_links_from_page <- function(page) {
     ## Retourne les pages "Index" en bas de page
-    return(get_bottom_links_from_page(page, col_number = 1))
+    return(bottom_links_from_page(page, col_number = 1))
 }
 
-get_index_links_from_page <- function(page) {
+index_links_from_page <- function(page) {
     ## Retourne les pages "Index" en bas de page
-    return(get_bottom_links_from_page(page, col_number = 2))
+    return(bottom_links_from_page(page, col_number = 2))
 }
 
-get_next_links_from_page <- function(page) {
+next_links_from_page <- function(page) {
     ## Retourne les pages "Index" en bas de page
-    return(get_bottom_links_from_page(page, col_number = 0))
+    return(bottom_links_from_page(page, col_number = 0))
 }
 
 ## MAIN
 page <- "https://tvtropes.org/pmwiki/pmwiki.php/WebVideo/TheOldestView"
 
-get_previous_links_from_page(page)
+subpage_links_from_page(page)
 
-get_index_links_from_page(page)
+previous_links_from_page(page)
 
-get_next_links_from_page(page)
+index_links_from_page(page)
+
+next_links_from_page(page)
